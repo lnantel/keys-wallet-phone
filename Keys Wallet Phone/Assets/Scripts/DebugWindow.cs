3eebce50f2a72323640fcf726eb02044893fdc73
Francis Rufiange <francis.rufiange@live.ca>
2021-01-30 15:37:47 -0500

    Merge master / dropsystem / inventory

---
+++ Keys Wallet Phone/Assets/Scripts/DebugWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugWindow : MonoBehaviour
+{
+    public static Action objectsReenable;
+    public static Action dropObject;
+    public GameObject debugCanvas;
+
+    public PickableObject keys;
+    public PickableObject wallet;
+    public PickableObject phone;
+
+    private void Start()
+    {
+        debugCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            debugCanvas.SetActive(!debugCanvas.activeSelf);
+        }
+    }
+
+    public void ReenableObjects()
+    {
+        if (objectsReenable != null)
+            objectsReenable();
+    }
+
+    public void PickupObject(string objectName)
+    {
+        switch (objectName)
+        {
+            case "Keys":
+                keys.Pickup();
+                break;
+            case "Wallet":
+                wallet.Pickup();
+                break;
+            case "Phone":
+                phone.Pickup();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void DropObject()
+    {
+        if (dropObject != null)
+            dropObject();
+    }
+}
---
+++ Keys Wallet Phone/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+    private List<PickableObject> _objectsInInventory = new List<PickableObject>();
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        PickableObject.objecPickedUp += AddToInventory;
+        DebugWindow.dropObject += DropRandomObject;
+    }
+
+    private void OnDisable()
+    {
+        PickableObject.objecPickedUp -= AddToInventory;
+        DebugWindow.dropObject -= DropRandomObject;
+    }
+
+    private void AddToInventory(PickableObject obj)
+    {
+        _objectsInInventory.Add(obj);
+        Debug.Log("Added : " + obj.objectName + " to inventory.");
+    }
+
+    private void RemoveFromInventory(PickableObject obj)
+    {
+        _objectsInInventory.Remove(obj);
+        Debug.Log("Removed : " + obj.objectName + " from inventory.");
+    }
+
+    // Function that checks if an object is picked up in inventory (uses object name/string)
+    private bool CheckForObject(string objectName)
+    {
+        var found = _objectsInInventory.Find(x => x.objectName == objectName);
+
+        if (found != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void DropRandomObject()
+    {
+        int objectCount = _objectsInInventory.Count;
+
+        // Don't execute if 0 objects
+        if (objectCount <= 0)
+            return;
+
+        int dropIndex = Random.Range(0, objectCount);
+
+        PickableObject objToDrop = _objectsInInventory[dropIndex];
+        RemoveFromInventory(objToDrop);
+
+        Vector3 RandomPos = GetRandomPosition();
+        objToDrop.Drop(RandomPos);
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        float x = Random.Range(-5f, 5f);
+        float y = Random.Range(1f, 2f);
+        float z = Random.Range(-5f, 5f);
+
+        return new Vector3(x, y, z);
+    }
+}
