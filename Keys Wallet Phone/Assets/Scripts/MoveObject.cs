a8165ac56525784752f4ba55ccbe195301d71648
Edouard R <edouard.raffis@umontreal.ca>
2021-01-30 19:50:22 -0500

    Added the ability to move objects

---
+++ Keys Wallet Phone/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{   
+    RaycastHit hit;
+    public int castDist;
+    public GameObject springConnection;
+    public GameObject grabbedObject;
+    public bool releaseTimer;
+    public bool hasSpringJoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        castDist = 5;
+        releaseTimer = true;
+        hasSpringJoint = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && grabbedObject != null ) ReleaseObject();
+
+        if(Input.GetMouseButtonDown(0) && grabbedObject == null && releaseTimer) GrabObject();
+    }
+
+    public void GrabObject()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out hit, castDist) && hit.transform.gameObject.layer == 10)
+        {
+            print("Object Grabbed");
+        
+            if(hit.transform.gameObject.GetComponent<SpringJoint>() == null) {
+                hasSpringJoint = false;
+                print("Doesn't have joint");
+            } else hasSpringJoint = true;
+
+            switch (hasSpringJoint)
+            {
+            
+                case false :
+                    hit.transform.gameObject.AddComponent<SpringJoint>().connectedBody = springConnection.GetComponent<Rigidbody>();
+                    grabbedObject = hit.transform.gameObject;
+                    break;
+
+                case true :
+                    hit.transform.gameObject.GetComponent<SpringJoint>().connectedBody = springConnection.GetComponent<Rigidbody>();
+                    grabbedObject = hit.transform.gameObject;
+                    break;
+            }
+            
+        }
+
+        else print("No object grabbed");
+    }
+
+    public void ReleaseObject()
+    {
+
+        //Destroy(grabbedObject.GetComponent<SpringJoint>().connectedBody);
+        grabbedObject.GetComponent<SpringJoint>().connectedBody = null;
+        grabbedObject = null;
+        hasSpringJoint = true;
+        print("Object Released");
+        StartCoroutine(WaitAfterRelease());
+    }
+
+    IEnumerator WaitAfterRelease()
+    {
+        releaseTimer = false;
+        yield return new WaitForSeconds(2);
+        releaseTimer = true;
+
+        
+    }
+
+}
